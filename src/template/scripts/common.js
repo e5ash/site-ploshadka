317ae75b4e7d80101c5cdd2646c62841e6073b1b
e5ash <e5ash.bro@gmail.com>
2018-09-09 07:45:18 +0300

    2 slides

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -2,4 +2,22 @@ $(document).ready(function($) {
 
 	$('.input_phone .input__wrap').mask('+7 (000) 000-00-00');
 
+	var panel = $('.panel'),
+			panelClass = 'panel_fixed';
+
+	$(window).on('load resize scroll', function(event) {
+		var windowScroll = $(window).scrollTop();
+
+		if (windowScroll >= 96) {
+			panel.addClass(panelClass);
+		} else{
+			panel.removeClass(panelClass);
+		}
+	});
+
+	// sliders
+
+	$('.teach__list').slick({
+		fade: true
+	});
 });
