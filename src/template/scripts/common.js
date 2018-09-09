a5ddabeab2af05d23f8e4ca8d6ea3e853a99ddac
e5ash <e5ash.bro@gmail.com>
2018-09-09 17:06:49 +0300

    Все до подарка - камеры

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -20,4 +20,44 @@ $(document).ready(function($) {
 	$('.teach__list').slick({
 		fade: true
 	});
+
+	$('.with__list').slick({
+		fade: true
+	})
+
+
+	$('.shares__btn').click(function(event) {
+		var sharesItem = $(this).parents('.shares__item'),
+				countArrea = sharesItem.find('.shares__count'),
+				countDesc = sharesItem.find('.shares__desc'),
+				countClick = Number(sharesItem.attr('data-count-click'));
+		
+
+		countArrea.addClass('shares__count_show')
+
+		setTimeout(function() {
+			countArrea.removeClass('shares__count_show');
+		}, 500);
+		
+
+		if (countClick < 2) {
+			window.open(sharesItem.attr('data-target-page'));
+		} else{
+			countClick--;
+			countArrea.html(countClick);
+			countDesc.each(function(index, elem) {
+				var count = Number($(this).attr('data-count-show'));
+				if (count == countClick) {
+					countDesc.removeClass('shares__desc_show');
+					$(this).addClass('shares__desc_show');
+				}
+			});
+			setTimeout(function() {
+				countDesc.removeClass('shares__desc_show');
+			}, 2000);
+			
+		}
+
+		sharesItem.attr('data-count-click', countClick);
+	});
 });
