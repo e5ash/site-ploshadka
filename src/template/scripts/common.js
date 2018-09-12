7282f0f11bf487ef3a5db5b8fd7f3057858ea7ec
e5ash <e5ash.bro@gmail.com>
2018-09-12 20:18:40 +0300

    add

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -60,4 +60,85 @@ $(document).ready(function($) {
 
 		sharesItem.attr('data-count-click', countClick);
 	});
+
+
+	var tableAnimation = $('.table:nth-of-type(1)'),
+			tableStatic = $('.table:nth-of-type(2)'),
+			linkTableAnimation = $('.schedule__link b'),
+			linkTableStatic = $('.schedule__link u');
+
+	tableStatic.hide();
+
+
+	function checkLink() {
+		if (tableAnimation.is(':visible')) {
+			linkTableStatic.show();
+			linkTableAnimation.hide();
+		} else{
+			linkTableStatic.hide();
+			linkTableAnimation.show();
+		}
+	}
+	
+	checkLink();
+
+	linkTableStatic.click(function() {
+		tableAnimation.hide();
+		tableStatic.show();
+		checkLink();
+	});
+
+	linkTableAnimation.click(function() {
+		tableAnimation.show();
+		tableStatic.hide();
+		checkLink();
+	});
+
+
+	$("a[href^='#']").click(function(){
+		var _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top - 70 +"px"}, 1500);
+		return false;
+	});
+
+
+	// slider
+	$slick_slider1 = $('.pride__list');
+	$slick_slider2 = $('.about__images');
+	settings_slider = {
+		dots: false,
+		// more settings
+	}
+	slick_on_mobile( $slick_slider1, settings_slider);
+	slick_on_mobile( $slick_slider2, settings_slider);
+
+	// slick on mobile
+		function slick_on_mobile(slider, settings){
+			$(window).on('load resize', function() {
+				if ($(window).width() > 767) {
+					if (slider.hasClass('slick-initialized')) {
+						slider.slick('unslick');
+					}
+					return
+				}
+				if (!slider.hasClass('slick-initialized')) {
+					return slider.slick(settings);
+				}
+			});
+	};
+
+	$('.table__group-name').click(function(event) {
+		var list = $(this).next('.table__group-list');
+		list.slideToggle(300);
+	});
+
+
+	var mobileBtn = $('.mobile-btn'),
+			nav = $('.panel__nav');
+
+	mobileBtn.click(function(event) {
+		$(this).toggleClass('mobile-btn_toggle');
+		nav.toggleClass('panel__nav_toggle');
+	});
+
 });
